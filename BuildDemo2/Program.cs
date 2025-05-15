1c5c91b87a3f26d78157d017c7e0242cc7c8a2c1
Martin Ruiz <martin.ruiz.mares@gmail.com>
2025-05-15 09:48:16 -0700

    Initial demo project

---
+++ BuildDemo2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BuildDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
